6fab6f65692316207072ace5d783b9fcc96ee26c
Jaap vd Belt <jaapvdbelt@gmail.com>
2014-10-31 15:21:16 +0100

    Initial commit

---
+++ HelloWorld/js/HelloWorld.js
@@ -0,0 +1,46 @@
+/**
+ * Created by Jaap on 24-10-2014.
+ */
+
+// Opdracht 1 a t/m c
+function helloWorld() {
+    return "Hallo Wereld, mijn eerste javascript functie";
+}
+console.log(helloWorld());
+
+// Opdracht1 d t/m f
+function mijnNaam() {
+    var mij;
+    mij = {
+        voorNaam : "Jaap",
+        achterNaam : "van de Belt"
+    };
+    return "Mijn naam is: " + mij.voorNaam + " " + mij.achterNaam;
+}
+console.log(mijnNaam());
+
+// Opdracht 2 Expressies
+function opdracht2() {
+    var x = '1';
+    var y = 1;
+    console.log(x + y); // Dit geeft 11 als uitvoer
+    console.log(x * y); // Dit geeft 1 als uitvoer
+    console.log(x == y); // Dit geeft true als uitvoer
+    console.log(x ? 2 : 3); // Dit geeft 2 als uitvoer want x = true
+    console.log(!y ? 2 : 3); // Dit geeft 3 als uitvoer want !y = false
+
+    return "Variabele expressies gedaan"
+}
+console.log(opdracht2());
+
+function opdracht3Contains(array, item) {
+    for (var i = 0; i < array.length; i++) {
+        if (array[i] === item) {
+            return true;
+        }
+    }
+    return false;
+}
+console.log("drie zit wel in deze array " +
+"resultaat van opdracht3Contains is: " +
+        opdracht3Contains(["een", "twee", "drie","vier", "vijf", "zes"], "drie"));
